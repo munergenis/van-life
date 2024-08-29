a172f2800f85cd42971e2d2cd89dffad98d1588f
Genis Muner <munergenis@gmail.com>
2024-08-29 20:58:28 +0200

    fix badge color parse

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,6 +1,6 @@
 const Badge = ({ children, className, colorStyle }) => {
   return (
-    <span className={`rounded-md px-4 py-2 text-center font-bold text-neutral-600 ${className} ${colorStyle || 'bg-orange-200'}`}>{children}</span>
+    <span className={`rounded-md px-4 py-2 text-center font-bold ${className} ${colorStyle || 'bg-orange-200 text-neutral-600'}`}>{children}</span>
   )
 }
 export default Badge
